e844dbbff0caf03a095bdba43aeac813f9b89057
Mohitash Kumar <devmohitashxda@gmail.com>
2021-07-06 16:39:50 +0530

    Add files via upload

--- se-xtreme/service.sh
+++ se-xtreme/service.sh
@@ -1,4 +1,19 @@
 #!/system/bin/sh
-set_perm_recursive "/data/adb/modules/se-xtreme/system/bin" root root 0777 0755
+
 # Setup tweaks
-sextremehelper
+BB=/data/adb/magisk/busybox
+CLOG=/data/media/0/YourSEchanges.log
+
+kmsg3() {
+	echo -e "[*] $@" >> $CLOG
+}
+
+$BB cp -rf /data/adb/modules/se-xtreme/system/etc/sextreme/sound_tuner.db /data/data/com.oneplus.sound.tuner/databases/sound_tuner.db
+
+$BB rm -rf /data/adb/modules/se-xtreme/system/etc
+
+kmsg3 $(date) 
+kmsg3 "And Helper Worked"
+kmsg3 "----"
+
+$BB rm -rf /data/adb/modules/se-xtreme/service.sh
